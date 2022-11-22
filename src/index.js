8cd2b69b00e478e42a1f7a36c4fe7e41c45065cb
sam <geeksam820@gmail.com>
2022-11-22 15:15:21 -0500

    custom comonent with jsx

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import App from './App'
 
-class App extends React.Component {
-    render() {
-      return (
-        <div className="App">
-          <h1>Hello, React step-by-step!</h1>
-        </div>
-      )
-    }
-  }
+import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
